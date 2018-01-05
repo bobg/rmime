3841cef921e87379a399c98ba97c465cec41cb85
Bob Glickstein <bobg@emphatic.com>
2018-01-05 07:38:26 -0800

    Implement message/delivery-status.

--- body.go
+++ body.go
@@ -9,12 +9,18 @@ import (
 
 var ErrUnimplemented = errors.New("unimplemented")
 
+type DeliveryStatus struct {
+	Message    *Header   `json:"message"`
+	Recipients []*Header `json:"recipients"`
+}
+
 // ReadBody reads a message body from r after the header has been read
 // and parsed. The type of the result depends on the content-type
 // specified in the header:
-//   message/rfc822: *Message
-//   multipart/*:    *Multipart
-//   */*:            string
+//   message/rfc822:          *Message
+//   message/delivery-status: *DeliveryStatus
+//   multipart/*:             *Multipart
+//   */*:                     string
 func ReadBody(r io.Reader, header *Header) (interface{}, error) {
 	switch header.MajorType() {
 	case "message":
@@ -29,7 +35,27 @@ func ReadBody(r io.Reader, header *Header) (interface{}, error) {
 			return nil, ErrUnimplemented
 
 		case "delivery-status":
-			return nil, ErrUnimplemented
+			// A message-level set of header fields, followed by one or more
+			// per-recipient sets of header fields (see RFC 3464).
+			perMessage, err := ReadHeader(r, "text/plain")
+			if err != nil {
+				return nil, err
+			}
+			var perRecipient []*Header
+			for {
+				h, err := ReadHeader(r, "text/plain")
+				if err == io.EOF {
+					break
+				}
+				if err != nil {
+					return nil, err
+				}
+				if len(h.Fields) == 0 {
+					break
+				}
+				perRecipient = append(perRecipient, h)
+			}
+			return &DeliveryStatus{perMessage, perRecipient}, nil
 
 		default:
 			return nil, fmt.Errorf("unknown message subtype %s", header.MinorType())
