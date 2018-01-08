886843eb0ff046dd7f2af7ee85d025f8ff7da35b
Bob Glickstein <bobg@emphatic.com>
2018-01-07 19:41:45 -0800

    Use htmlindex rather than homegrown charset-name canonicalization.

--- charset.go
+++ charset.go
@@ -4,25 +4,20 @@ import (
 	"io"
 	"strings"
 
+	"golang.org/x/text/encoding/htmlindex"
 	"golang.org/x/text/encoding/ianaindex"
 )
 
-// Map from common non-canonical charset names to canonical ones.
-var canonicalCharsets = map[string]string{
-	"ascii":  "us-ascii",
-	"cp1252": "cp-1252",
-}
-
 func charsetReader(label string, inp io.Reader) (io.Reader, error) {
 	label = strings.ToLower(strings.TrimSpace(label))
-	if l, ok := canonicalCharsets[label]; ok {
-		label = l
-	}
 	enc, err := ianaindex.MIME.Encoding(label)
 	if err != nil {
 		enc, err = ianaindex.IANA.Encoding(label)
 		if err != nil {
-			return nil, wrapf(err, "charset %s", label)
+			enc, err = htmlindex.Get(label)
+			if err != nil {
+				return nil, wrapf(err, "charset %s", label)
+			}
 		}
 	}
 	if enc == nil {
